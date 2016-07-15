4cc6224ba4f0db3610f558e923c957375495494b
Refa Andhika <refandika@gmail.com>
2016-07-15 12:41:55 +0700

    Fixing CSS

--- application/views/media_online/media_report.php
+++ application/views/media_online/media_report.php
@@ -3,17 +3,7 @@
 							<h3 class="report-title">
 								<img src="assets/images/icon-title2.png">Chart Report</h3>
 								<div class="report-content-chart">
-								<table>
-									<thead>
-										<tr>
-											<th>Media</th>
-											<th>Counter</th>
-										</tr>
-									</thead>
-									<tbody id='chart_count_report'>
-										
-									</tbody>
-								</table>
+								
 								</div>
 							</div>
 							<div class="btn btn-orange btn-lg">Download Chart Report</div>
