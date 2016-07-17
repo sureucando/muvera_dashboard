eeb7a4186b084cd8b68418d2effd24f3feef299c
sureucando <samuel_joelian@yahoo.com>
2016-07-17 18:46:50 +0700

    add pdf and png download

--- application/views/media_online/media_report.php
+++ application/views/media_online/media_report.php
@@ -3,13 +3,15 @@
 							<h3 class="report-title">
 								<img src="assets/images/icon-title2.png">Chart Report</h3>
 								<div class="report-content-chart" id="pie_chart_visualisation">
-								
-								</div>
+								</div>															
+								<canvas id="canvas" width="960" height="500" style="display:none"></canvas>		
+								<div id="svgdataurl" style="display:none"></div>
+								<div id="pngdataurl" style="display:none"></div>
 							</div>
 							<div class="btn btn-orange btn-lg">Download Chart Report</div>
 							<div class="btn-rnd-cont">
-								<div class="btn btn-orange btn-rnd"><img src="assets/images/file-pdf.png"></div>
-								<div class="btn btn-orange btn-rnd"><img src="assets/images/file-png.png"></div>
+								<div class="btn btn-orange btn-rnd" id="downloadPDF" onclick="return xepOnline.Formatter.Format('resultChart',{srctype:'svg',render:'download',filename:'d3donut'});"><img src="assets/images/file-pdf.png"></div>
+								<div class="btn btn-orange btn-rnd" id="downloadPNG" onclick="downloadPNG()"><img src="assets/images/file-png.png"></div>
 							</div>
 						</div>
 						<div class="file-container">
