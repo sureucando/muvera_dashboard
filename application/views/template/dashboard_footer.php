7a12c3f2160029a28e42106b7b36c45c0cae158c
Refa Andhika <refandika@gmail.com>
2016-07-18 23:12:43 +0700

    Fix conflict

--- application/views/template/dashboard_footer.php
+++ application/views/template/dashboard_footer.php
@@ -14,18 +14,12 @@
 
 				<!-- Aditional Div's -->
 				<!-- Loading Div -->
-<<<<<<< HEAD
 				<div id="loading"><img src="assets/images/loader.gif"></div>
 
-=======
-				<div id="loading"></div>
 				<input type='hidden' id='pdffile' value=""></input> 
 				<input type='hidden' id='pngfile' value=""></input> 
-				<input type='hidden' id='xlsfile' value=""></input> 
-				<script src="assets/js/jquery-1.12.3.js"></script>
-				<script src="assets/js/jquery-ui.js"></script>
-				<script src="assets/js/assets.js"></script>
->>>>>>> refs/remotes/sureucando/master
+				<input type='hidden' id='xlsfile' value=""></input>
+
 				<script src="https://d3js.org/d3.v3.min.js"></script>
 				<script src="assets/js/jspdf.js"></script> 
 				<script src="assets/js/addimage.js"></script> 
@@ -40,32 +34,7 @@
 				<script type="text/javascript"> 
 					
 					
-<<<<<<< HEAD
 					function downloadPDF(){
-						//can print pdf with low quality
-						/*var svgChart = new XMLSerializer().serializeToString(document.getElementById('ResultChart'));
-						var svgLeged = new XMLSerializer().serializeToString(document.getElementById('ResultLegend'));
-						var canvas = document.getElementById("canvas");
-						var ctx = canvas.getContext("2d");
-						var DOMURL = self.URL || self.webkitURL || self;
-						var imgChart = new Image();
-						var imgLegend = new Image();
-						var svgCh = new Blob([svgChart], {type: "image/svg+xml;charset=utf-8"});
-						var svgLg = new Blob([svgLeged], {type: "image/svg+xml;charset=utf-8"});
-						var urlChart = DOMURL.createObjectURL(svgCh);
-						var urlLegend = DOMURL.createObjectURL(svgLg);
-						var count = 2;
-						imgChart.onload = imgLegend.onload = function() {
-							count --;
-							if (count === 0) drawPDF(ctx, imgChart, imgLegend, canvas, DOMURL);
-						};
-						imgChart.src = urlChart;
-						imgLegend.src = urlLegend;*/
-						
-						$('#loading').show();
-=======
-					function downloadPDF(){						
->>>>>>> refs/remotes/sureucando/master
 						var url = "<?php echo base_url('print_report/printpdf')?>";
 						$.ajax({
 							url: url,
@@ -166,15 +135,11 @@
 							error: function (jqXHR, textStatus, errorThrown)
 							{
 								alert('Error !');
-<<<<<<< HEAD
 								$('#loading').hide();
-=======
->>>>>>> refs/remotes/sureucando/master
 							}
 						});
 					}
 					
-<<<<<<< HEAD
 					function drawPDF(ctx, img1, img2,canvas, DOMURL){
 						$('#loading').show();
 						ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -185,31 +150,6 @@
 						pdf.addImage(img, 'PNG', 10, 10);
 						pdf.save("download.pdf");
 						$('#loading').hide();
-=======
-					function drawImages2(ctx, img1, img2,canvas, DOMURL){
-						ctx.clearRect(0, 0, canvas.width, canvas.height);
-						ctx.drawImage(img1, 0, 0,500,300);
-						var png = canvas.toDataURL("image/png");
-						var url = "<?php echo base_url('print_report/printpng')?>";
-						$.ajax({
-							url: url,
-							type : "POST",
-							data : png,
-							contentType : "text",
-							success : function(data){
-								if (data && data.search('filename') != -1){
-									var arrDispo = data.split(':');
-									var filename = arrDispo[1].trim();
-									document.getElementById('pngfile').value = filename;
-									downloadXLS2();
-								}
-							},
-							error: function (jqXHR, textStatus, errorThrown)
-							{
-								alert('Error !');
-							}
-						});
->>>>>>> refs/remotes/sureucando/master
 					}
 										
 					function downloadXLS(){
@@ -234,10 +174,7 @@
 								error: function (jqXHR, textStatus, errorThrown)
 								{
 									alert('Error !');
-<<<<<<< HEAD
 									$('#loading').hide();
-=======
->>>>>>> refs/remotes/sureucando/master
 								}
 							});
 						}
@@ -295,6 +232,7 @@
 							});
 						
 					}
+
 					function search(){
 						var valid = validateForm();
 						if (valid){
@@ -335,6 +273,30 @@
 						}
 					}
 					
+					function drawImages2(ctx, img1, img2,canvas, DOMURL){
+						ctx.clearRect(0, 0, canvas.width, canvas.height);
+						ctx.drawImage(img1, 0, 0,500,300);
+						var png = canvas.toDataURL("image/png");
+						var url = "<?php echo base_url('print_report/printpng')?>";
+						$.ajax({
+							url: url,
+							type : "POST",
+							data : png,
+							contentType : "text",
+							success : function(data){
+								if (data && data.search('filename') != -1){
+									var arrDispo = data.split(':');
+									var filename = arrDispo[1].trim();
+									document.getElementById('pngfile').value = filename;
+									downloadXLS2();
+								}
+							},
+							error: function (jqXHR, textStatus, errorThrown)
+							{
+								alert('Error !');
+							}
+						});
+					}
 				</script>
 </body>
 </html>
