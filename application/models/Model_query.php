924aeaaf9805e6afb4eb5bcf315bc85ae8d27315
sureucando <samuel_joelian@yahoo.com>
2016-07-21 11:49:22 +0700

    add query for line chart function

--- application/models/Model_query.php
+++ application/models/Model_query.php
@@ -16,6 +16,16 @@ require_once(APPPATH.'third_party/ChromePhp.php');
 			}
 		}
 		
+		function getCountTimeSeriesBasedKeyword($k1, $k2, $k3, $k4, $k5, $datefrom, $dateto, $table_name){
+			$query = $this->db->query("select date(date) as tanggal, count(*) as total from $table_name where content like '%$k1%' and content like '%$k2%' and content like '%$k3%' and content like '%$k4%' and content like '%$k5%' and date between '$datefrom' and '$dateto' group by date(date) ");
+			if ($query->num_rows() > 0) {
+				return $query->result();
+			}
+			else {
+				return NULL;
+			}
+		}
+		
 		function getRawData($k1, $k2, $k3, $k4, $k5, $datefrom, $dateto, $table_name,$limit){
 			$query = $this->db->query("select title,date,link,content from $table_name where content like '%$k1%' and content like '%$k2%' and content like '%$k3%' and content like '%$k4%' and content like '%$k5%' and date between '$datefrom' and '$dateto' limit $limit");
 			if ($query->num_rows() > 0) {
