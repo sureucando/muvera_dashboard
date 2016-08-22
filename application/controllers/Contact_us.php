5660d3e2b067b2dc5c76773a71d0a0b0677f56e4
Refa Andhika <refandika@gmail.com>
2016-08-22 12:43:15 +0700

    Update contact us header

--- application/controllers/Contact_us.php
+++ application/controllers/Contact_us.php
@@ -5,10 +5,26 @@ class Contact_us extends CI_Controller {
 
 	public function index()
 	{
-		$this->home();
+		if ( ! $this->session->userdata('logged_in'))
+        {
+			$this->loggedOut();
+        }
+        else
+        {
+			$this->loggedIn();
+        }
 	}
 
-	public function home()
+	public function loggedIn()
+	{
+		$data['title'] = 'Contact Us';
+
+		$this->load->view('template/dashboard_headerv2.php', $data);
+		$this->load->view('contact_us/contact_us.php', $data);
+		$this->load->view('template/dashboard_footer.php', $data);
+	}
+
+	public function loggedOut()
 	{
 		$data['title'] = 'Contact Us';
 
